732e9fc805e0543a5d3b68c7ea42eeaa5314dc42
Leat02 <20520948@gm.uit.edu.vn>
2023-10-19 23:03:44 +0700

    push len git

---
+++ app/src/main/java/com/example/bigproject/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.bigproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_main);
+
+        Intent intent = new Intent(this, logo.class);
+        startActivity(intent);
+    }
+
+}
+
+
