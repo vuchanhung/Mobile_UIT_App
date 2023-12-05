08ac593effd63a595c5147189314d9961e9c16fc
ThongHa <haminhthong0811@gmail.com>
2023-12-05 20:53:52 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
